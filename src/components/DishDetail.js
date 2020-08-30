a390c850de5f1d6e6dc080316d0dc21d1cc3f846
niquito <ncaffarini@gmail.com>
2020-08-30 19:09:15 -0300

    Create DishDetail.js

---
+++ src/components/DishDetail.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { Card, CardImg, CardImgOverlay, CardBody, CardTitle, CardText } from 'reactstrap';
+
+class DishDetail extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+
+		const dish = this.props.dish;
+
+		return (
+			<div className="row">
+				<Card>
+					<CardImg width="100%" src={dish.image} alt={dish.name} />
+					<CardBody>
+						<CardTitle>{dish.name}</CardTitle>
+						<CardText>{dish.description}</CardText>
+					</CardBody>
+				</Card>
+			</div>
+		);
+	}
+
+}
+
+export default DishDetail;
